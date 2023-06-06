1fc0fc5bf12daf273fb8f5da72b34b642c48120e
Felix Brown <thibeauxjohnson@gmail.com>
2023-06-06 03:47:03 +0100

    Minor 3am additions.

--- linked-hashmap.c
+++ linked-hashmap.c
@@ -2,19 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-LinkedHashmap *new_LinkedHashmap() {
-    LinkedHashmap *this = malloc(sizeof(LinkedHashmap));
-    init_LinkedHashmap(this);
-    return this;
-}
-
-void init_LinkedHashmap(LinkedHashmap *this) {
-    this -> table = (Entry*) malloc(INITIAL_CAPACITY * sizeof(Entry));
-    this -> getHash = FNV1A_Hash;
-    this -> head = NULL;
-    this -> tail = NULL;
-}
-
 static uint16_t FNV1_Hash(uint16_t thread_id) {
     uint32_t hash = FNV_OFFSET_BASIS;
     for (int i = 0; i < 32; i += 8) {
@@ -35,4 +22,41 @@ static uint16_t FNV1A_Hash(uint16_t thread_id) {
 
     hash = (uint16_t) (hash >> 16) ^ (hash & MASK_16);
     return hash;
+}
+
+LinkedHashmap *new_LinkedHashmap() {
+    LinkedHashmap *this = malloc(sizeof(LinkedHashmap));
+    if (this == NULL) {
+        return NULL;
+    }
+    init_LinkedHashmap(this);
+    return this;
+}
+
+int init_LinkedHashmap(LinkedHashmap *this) {
+    this -> entries_count = 0;
+    this -> table_size = INITIAL_CAPACITY;
+    this -> load_factor = 0.0;
+    this -> head = NULL;
+    this -> tail = NULL;
+    this -> table = (Entry*) malloc(INITIAL_CAPACITY * sizeof(Entry));
+    if (table == NULL) {
+        return 0;
+    }
+    this -> get_hash = FNV1A_Hash;
+    this -> free = free_table;
+    return 1;
+}
+
+static void free_table(LinkedHashmap *this) {
+    free(this -> table);
+    free(this);
+}
+
+// enqueue
+static thread *insert(thread * t, LinkedHashmap * lhm) {
+    uint16_t table_index;
+    const uint16_t thread_id = t -> id;
+    table_index = lhm -> getHash(thread_id) & ((lhm -> table_size) - 1);
+
 }
--- linked-hashmap.h
+++ linked-hashmap.h
@@ -5,7 +5,7 @@
 typedef unsigned short uint16_t; // defined in types.h
 typedef unsigned int uint32_t;
 
-#define INITIAL_CAPACITY 16
+#define INITIAL_CAPACITY 32
 #define FNV_OFFSET_BASIS 2166136261 // http://www.isthe.com/chongo/tech/comp/fnv/#FNV-param
 #define FNV_PRIME 16777619
 #define MASK_16 (((uint32_t) 1<<16) -1)
@@ -28,23 +28,31 @@ struct Entry {
     thread * t;
 };
 
+//Entry *new_Entry(thread *t);
+//void init_Entry(Entry *this);
+
 struct LinkedHashmap {
     // Common Queue Interface
-    thread* (*dequeue) (); // how to indicate failure? special pointer value?
-    void (*removeByID) (uint16_t);
+    thread* (*dequeue) (void);
+    thread* (*removeByID) (uint16_t);
     int (*contains) (uint16_t); // bool-ish
-    int (*enqueue) (thread*);
-    int (*isEmpty) ();
+    int (*enqueue) (thread*); // success/failure return value
+    int (*isEmpty) (void); // bool-ish
     // Linked hashmap only
+    int entries_count;
+    int table_size;
+    double load_factor;
     Entry * head;
     Entry * tail;
     Entry * table; // malloc table
-    uint16_t (*getHash) (uint16_t);
+    uint16_t (*get_hash) (uint16_t);
+    void (*free) (LinkedHashmap*)
 };
 
 
 LinkedHashmap *new_LinkedHashmap();
-void init_LinkedHashmap(LinkedHashmap *this);
+int init_LinkedHashmap(LinkedHashmap *this); // success1/failure0 return value
+
 
 
 #endif /* LINKED_HASHMAP_H */
