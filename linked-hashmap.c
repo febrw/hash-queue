5a4d240944d8df19dd8e43ae09e0226e530784a0
Felix Brown <ftjb1@st-andrews.ac.uk>
2023-06-22 23:14:18 +0100

    Rehashing + Enqueue first attempt

--- linked-hashmap.c
+++ linked-hashmap.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//---------------------------------------- HASH FUNCTIONS ----------------------------------------
 static u16 FNV1_Hash(u16 thread_id) {
     u32 hash = FNV_OFFSET_BASIS;
     for (int i = 0; i < 32; i += 8) {
@@ -24,8 +25,10 @@ static u16 FNV1A_Hash(u16 thread_id) {
     return hash;
 }
 
+//----------------------------------- CONSTRUCTORS + DESTRUCTOR -----------------------------------
 
-LinkedHashmap *new_LinkedHashmap() {
+
+LinkedHashmap *new_def_LinkedHashmap() {
     LinkedHashmap *this = malloc(sizeof(LinkedHashmap));
     if (this == NULL) {
         return NULL;
@@ -34,22 +37,26 @@ LinkedHashmap *new_LinkedHashmap() {
     return this;
 }
 
-int init_LinkedHashmap(LinkedHashmap *this) {
-    this -> entries_count = 0;
-    this -> table_size = INITIAL_CAPACITY;
+
+/*
+    Returns 0 if any malloc failed, 1 otherwise.
+*/
+int init_def_LinkedHashmap(LinkedHashmap *this) {
+    this -> size = 0;
+    this -> capacity = INITIAL_CAPACITY;
     this -> load_factor = 0.0;
     this -> resize_threshold = REHASH_THRESHOLD;
     this -> head = NULL;
     this -> tail = NULL;
-    this -> table = malloc(INITIAL_CAPACITY * sizeof(Entry));
+    this -> table = malloc(INITIAL_CAPACITY * sizeof(Entry*));
     if (this -> table == NULL) {
         return 0;
     }
-    this -> occupied_index = malloc((INITIAL_CAPACITY >> 3) * sizeof(u8));
+    this -> occupied_index = malloc((INITIAL_CAPACITY) * sizeof(u8));
     if (this -> occupied_index == NULL) {
         return 0;
     }
-    for (int i = 0; i < INITIAL_CAPACITY >> 3; ++i) {
+    for (int i = 0; i < INITIAL_CAPACITY; ++i) {
         this -> occupied_index[i] = 0; // all unoccupied to begin
     }
     this -> getHash = FNV1A_Hash;
@@ -57,23 +64,129 @@ int init_LinkedHashmap(LinkedHashmap *this) {
     return 1;
 }
 
+/*
+    - Doubles the table size
+    - Copies each Entry pointer into its new table slot
+    - Sets occupied fields to zero in old table so entries are not freed
+    - Frees the old table
+
+*/
+int init_rehashed_LinkedHashmap(LinkedHashmap* old, LinkedHashmap* new) {
+    // New table initialisation
+    new -> getHash = FNV1A_Hash;
+    new -> free = free_table;
+    new -> capacity = old -> capacity * 2;
+    new -> resize_threshold = REHASH_THRESHOLD;
+    new -> head = old -> head;
+    new -> tail = old -> tail;
+    new -> table = malloc((new -> capacity) * sizeof(Entry*));
+    if (new -> table == NULL) {
+        return 0;
+    }
+
+    new -> occupied_index = malloc((new -> capacity) * sizeof(u8));
+    if (new -> occupied_index == NULL) {
+        return 0;
+    }
+
+    for (int i = 0; i < new -> capacity; ++i) {
+        new -> occupied_index[i] = 0;
+    }
+
+    // Rehashing Procedure
+    const u16 table_mask = (new -> capacity) - 1;
+    u16 table_index;
+    for (int i = 0; i < old -> capacity; ++i) {
+        if (old -> occupied_index[i] != 0) {
+            Entry * entry = old -> table[i];
+
+            table_index = (entry -> hash) & table_mask;
+            while (new -> occupied_index[table_index] != 0) {
+                table_index = (table_index + 1) & table_mask;
+            }
+            new -> table[table_index] = entry;
+            new -> occupied_index[table_index] = 1;
+
+            old -> occupied_index[i] = 0; // set to zero so entries will not be freed
+        }
+    }
+
+    free_table(old);
+
+    return 1;
+}
+
+/*
+    - Frees the entries in a table provided they are marked as occupied
+*/
 static void free_table(LinkedHashmap *this) {
+    // Free each entry in the hash table
+    for (int i = 0; i < this -> capacity; ++i) {
+        if (this -> occupied_index[i] != 0) {
+            free(this -> table[i]);
+        }
+    }
     free(this -> table);
+    free(this -> occupied_index);
     free(this);
 }
 
-// enqueue
-static thread *enqueue(thread * t, LinkedHashmap * lhm) {
+//------------------------------ LINKED HASHMAP ADT IMPLEMENTATIONS ------------------------------
+
+/*
+    Returns -1 if enqueue failed, 1 if succeeded, 2 if succeeded but rehashing required.
+*/
+static int enqueue(thread * t, void * queue) {
+    LinkedHashmap * map = (LinkedHashmap*) queue;
     const u16 thread_id = t -> id;
-    u16 table_index = lhm -> getHash(thread_id) & ((lhm -> table_size) - 1);
+    const u16 table_mask = (map -> capacity) - 1;
+    const u16 hash = map -> getHash(thread_id);
+    u16 table_index = hash & table_mask;
 
-    while (lhm -> table) {
-
+    int slots_searched = 0;
+    while (map -> occupied_index[table_index] != 0 && slots_searched < map -> capacity) // iterate while positions unavailable
+    {
+        table_index = (table_index + 1) & table_mask;
+        ++slots_searched;
     }
 
+    if (slots_searched == map -> capacity) {
+        printf("Table somehow full.\n");
+        return -1;
+    }
+
+    // Create new entry
+    Entry * new_entry = malloc(sizeof(Entry));
+    if (new_entry == NULL) {
+        printf("Entry malloc failed.\n");
+        return -1;
+    }
+
+    new_entry -> t = t;
+    new_entry -> hash = hash;
+    
+    // Linked List pointers update
+    new_entry -> prev = map -> tail;
+    new_entry -> next = NULL;
+    map -> tail -> prev = new_entry;
+    map -> tail = new_entry;
+
+    map -> table[table_index] = new_entry;
+    map -> occupied_index[table_index] = 1;
+    ++ map -> size;
+
+    map -> load_factor = (double) map -> size / map -> capacity;
+    if (map -> load_factor > REHASH_THRESHOLD) {
+        return 2;
+    } else {
+        return 1;
+    }
 }
 
 // dequeue
 static thread *dequeue(void) {
 
 }
+
+
+
--- linked-hashmap.h
+++ linked-hashmap.h
@@ -6,8 +6,8 @@ typedef unsigned short u16; // defined in types.h
 typedef unsigned int u32;
 typedef unsigned char u8;
 
-#define INITIAL_CAPACITY 32
-#define REHASH_THRESHOLD 0.8
+#define INITIAL_CAPACITY 64
+#define REHASH_THRESHOLD 0.75
 #define FNV_OFFSET_BASIS 2166136261 // http://www.isthe.com/chongo/tech/comp/fnv/#FNV-param
 #define FNV_PRIME 16777619
 #define MASK_16 (((u32) 1<<16) -1)
@@ -28,36 +28,35 @@ struct Entry {
     Entry * next;
     thread * t; // value
     u16 hash; // key
-    
 };
 
 
 
 struct LinkedHashmap {
     // Common Queue Interface
-    thread* (*dequeue) (void);
-    thread* (*removeByID) (u16);
-    int (*contains) (u16); // bool-ish
-    int (*enqueue) (thread*); // success/failure return value
-    int (*isEmpty) (void); // bool-ish
+    thread* (*dequeue) (void*);
+    thread* (*removeByID) (u16, void*);
+    thread* (*contains) (u16, void*); // NULL if does not contain
+    int (*enqueue) (thread*, void*); // success/failure return value
+    int (*isEmpty) (void*); // bool-ish
     // Linked hashmap only
-    int entries_count;
-    int table_size; // must be a power of 2
+    int size;
+    int capacity; // must be a power of 2
     double load_factor; // [0,1]
     double resize_threshold; // rehash when surpassed
     Entry * head;
     Entry * tail;
-    Entry * table; // malloc table
-    u8 * occupied_index; // bitfields indicating occupied / not occupied
+    Entry ** table; // malloc table, uses double pointers to allow rehashing to maintain next and prev pointers
+    u8 * occupied_index; // char array indicating slot occupations
     u16 (*getHash) (u16);
     void (*free) (LinkedHashmap*);
-
 };
 
 
-LinkedHashmap *new_LinkedHashmap();
-int init_LinkedHashmap(LinkedHashmap *this); // success1/failure0 return value
-
+LinkedHashmap *new_def_LinkedHashmap();
+LinkedHashmap *new_rehashed_LinkedHashmap(LinkedHashmap *);
+int init_def_LinkedHashmap(LinkedHashmap *); // success1/failure0 return value
+int init_rehashed_LinkedHashmap(LinkedHashmap*, LinkedHashmap*); 
 
 
 #endif /* LINKED_HASHMAP_H */
